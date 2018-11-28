cadda72b2896e6ff0e166cf5411c2b41cc240785
Kelvin <KelvinWongNYC@gmail.com>
2018-11-28 15:57:45 -0500

    Implemented search and create account

--- src/cse305database/FXMLDocumentController.java
+++ src/cse305database/FXMLDocumentController.java
@@ -5,10 +5,17 @@
  */
 package cse305database;
 
+import static cse305database.CSE305Database.searchActors;
+import static cse305database.CSE305Database.searchDirectors;
+import static cse305database.CSE305Database.searchMovies;
 import static cse305database.IMBD.s;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,7 +28,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -48,6 +58,19 @@ public class FXMLDocumentController implements Initializable {
        logout.setVisible(true);
     }
     
+    public static void setWelcomeText(String user){
+        Scene s1 = s.getScene();
+        Text t = (Text)s1.lookup("#welcome");
+        t.setVisible(true);
+        t.setText("Welcome " + user+ "!");
+        
+        
+        Button b = (Button)s1.lookup("#button");
+       b.setVisible(false);
+        Button logout = (Button)s1.lookup("#button1");
+       logout.setVisible(true);
+    }
+    
     @FXML
     private void logout(ActionEvent event) {
         Scene s1 = s.getScene();
@@ -63,23 +86,190 @@ public class FXMLDocumentController implements Initializable {
        //Remove Label!
     }
     
+    private static boolean movieSearch = true;
+    private static boolean actorSearch = false;
+    private static boolean directorSearch = false;
+
+    
     @FXML
     private void movie(ActionEvent event) {
         Scene s1 = s.getScene();
-   SplitMenuButton split = (SplitMenuButton)s1.lookup("#menuButton");
+        SplitMenuButton split = (SplitMenuButton)s1.lookup("#menuButton");
         split.setText("Movie"); 
+        movieSearch=true;
+        actorSearch=false;
+        directorSearch=false;
     }
      @FXML
      private void actor(ActionEvent event) {
          Scene s1 = s.getScene();
    SplitMenuButton split = (SplitMenuButton)s1.lookup("#menuButton");
         split.setText("Actor"); 
+         movieSearch=false;
+        actorSearch=true;
+        directorSearch=false;
     }
       @FXML
      private void director(ActionEvent event) {
          Scene s1 = s.getScene();
    SplitMenuButton split = (SplitMenuButton)s1.lookup("#menuButton");
         split.setText("Director"); 
+         movieSearch=false;
+        actorSearch=false;
+        directorSearch=true;
+    }
+     
+     @FXML
+    private void search(MouseEvent event) throws IOException{
+        Scene s1 = s.getScene();
+        TextField split = (TextField)s1.lookup("#searchBar");
+        String searchBar = split.getText();
+        
+        if(actorSearch){
+            
+          Parent root = FXMLLoader.load(getClass().getResource("Search.fxml"));
+        Scene scene = new Scene(root);
+        
+         Stage actor = new Stage();
+         actor.setTitle("Actor Search");
+          Text actorResults = (Text)scene.lookup("#searchResultsActor");
+        actorResults.setText("Search Results for \"" + searchBar + "\"");
+        
+        TableView actorTable = (TableView)scene.lookup("#actorTable");
+        actorTable.setPlaceholder(new Label("No search results for \"" + searchBar + "\""));
+        ArrayList<Actor> actorArrayList;
+        
+        //COMMENT OUT THIS PART OF THE CODE!!!
+        actorArrayList = new ArrayList<>();
+        Date testDate = new Date();
+        Actor testActor = new Actor(0, "Name",100,testDate,"Male");
+        actorArrayList.add(testActor);
+        
+        //actorArrayList = searchActors(searchBar);
+        if(actorArrayList.size() !=0){
+             ObservableList<Actor> ob = FXCollections.observableArrayList(actorArrayList);
+            TableColumn name =  (TableColumn) actorTable.getColumns().get(0);
+            name.setCellValueFactory(
+            new PropertyValueFactory<Actor,String>("name")
+               );
+            TableColumn height =  (TableColumn) actorTable.getColumns().get(1);
+             height.setCellValueFactory(
+            new PropertyValueFactory<Actor,Integer>("height")
+               );
+            TableColumn birthday =  (TableColumn) actorTable.getColumns().get(2);
+             birthday.setCellValueFactory(
+            new PropertyValueFactory<Actor,Date>("birthday")
+               );
+            TableColumn gender =  (TableColumn) actorTable.getColumns().get(3);
+             gender.setCellValueFactory(
+            new PropertyValueFactory<Actor,String>("gender")
+               );
+             actorTable.setItems(ob);
+        }
+        actor.setScene(scene);
+        actor.show();
+            
+        }else if(movieSearch){
+            
+         Parent root = FXMLLoader.load(getClass().getResource("SearchMovie.fxml"));
+        Scene scene = new Scene(root);
+         Stage movie = new Stage();
+         movie.setTitle("Movie Search");
+          Text movieResults = (Text)scene.lookup("#searchResultsMovie");
+        movieResults.setText("Search Results for \"" + searchBar + "\"");
+        
+        TableView movieTable = (TableView)scene.lookup("#movieTable");
+        movieTable.setPlaceholder(new Label("No search results for \"" + searchBar + "\""));
+        ArrayList<Movie> movieArrayList;
+        
+        //Comment out this part here
+        movieArrayList = new ArrayList<>();
+        Movie testMovie = new Movie(0,"TEST", "DIRECTOR", "FICTIOn",5.0,"Adult",2,2006,"null");
+        movieArrayList.add(testMovie);
+        
+        //movieArrayList = searchMovies(searchBar);
+        if(movieArrayList.size() != 0){
+            
+            ObservableList<Movie> ob = FXCollections.observableArrayList(movieArrayList);
+            TableColumn name =  (TableColumn) movieTable.getColumns().get(0);
+            name.setCellValueFactory(
+            new PropertyValueFactory<Movie,String>("name")
+               );
+             TableColumn director =  (TableColumn) movieTable.getColumns().get(1);
+            director.setCellValueFactory(
+            new PropertyValueFactory<Movie,String>("directorName")
+               );
+            
+             TableColumn genre =  (TableColumn) movieTable.getColumns().get(2);
+            genre.setCellValueFactory(
+            new PropertyValueFactory<Movie,String>("genre")
+               );
+            
+             TableColumn rating =  (TableColumn) movieTable.getColumns().get(3);
+            rating.setCellValueFactory(
+            new PropertyValueFactory<Movie,Double>("rating")
+               );
+            
+             TableColumn maturityRating =  (TableColumn) movieTable.getColumns().get(4);
+            maturityRating.setCellValueFactory(
+            new PropertyValueFactory<Movie,String>("maturityRating")
+               );
+            
+             TableColumn duration =  (TableColumn) movieTable.getColumns().get(5);
+            duration.setCellValueFactory(
+            new PropertyValueFactory<Movie,Integer>("duration")
+               );
+        
+             TableColumn releaseDate =  (TableColumn) movieTable.getColumns().get(6);
+            releaseDate.setCellValueFactory(
+            new PropertyValueFactory<Movie,Integer>("releaseDate")
+               );
+            movieTable.setItems(ob);
+        }
+        
+        
+        movie.setScene(scene);
+        movie.show();
+        
+        }else if(directorSearch){
+                Parent root = FXMLLoader.load(getClass().getResource("SearchDirector.fxml"));
+        Scene scene = new Scene(root);
+         Stage director = new Stage();
+         director.setTitle("Director Search");
+         Text directorResults = (Text)scene.lookup("#searchResultsDirector");
+        directorResults.setText("Search Results for \"" + searchBar + "\"");
+        
+        TableView directorTable = (TableView)scene.lookup("#directorTable");
+        directorTable.setPlaceholder(new Label("No search results for \"" + searchBar + "\""));
+        ArrayList<Director> directorArrayList;
+        
+        //COMMENT THIS PART BELOW
+        directorArrayList = new ArrayList<>();
+        directorArrayList.add(new Director(0, "NAME", 100));
+     
+        //directorArrayList = searchDirectors(searchBar);
+        
+        if(directorArrayList.size() != 0){
+            ObservableList<Director> ob = FXCollections.observableArrayList(directorArrayList);
+            TableColumn name =  (TableColumn) directorTable.getColumns().get(0);
+            name.setCellValueFactory(
+            new PropertyValueFactory<Movie,String>("name")
+               );
+            TableColumn age =  (TableColumn) directorTable.getColumns().get(1);
+            age.setCellValueFactory(
+            new PropertyValueFactory<Movie,String>("age")
+               );
+            
+            directorTable.setItems(ob);
+            
+        }
+     
+        
+        director.setScene(scene);
+        director.show();
+        
+        }
+        split.clear();
     }
     
      @FXML
@@ -122,10 +312,7 @@ public class FXMLDocumentController implements Initializable {
          ImageView jeff = (ImageView) s1.lookup("#jeff");
         jeff.setVisible(true);    
     }
-    @FXML
-    private void search(MouseEvent event){
-        clearScreen(event);
-    }
+    
      @FXML
      private void clearScreen(MouseEvent event) {
         Scene s1 = s.getScene();
